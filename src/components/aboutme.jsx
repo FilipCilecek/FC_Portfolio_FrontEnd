74d09ebb2da50beb479a2c11f495d3074d6cb648
Filip Cileček <51411215+FilipCilecek@users.noreply.github.com>
2022-10-21 17:03:07 +0200

    About me info

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -4,8 +4,13 @@ import '../App.js';
 function Aboutme() {
   return (
     <>
-      <span>ABOUT ME PAGE</span>
-      <span>FC</span>
+    {/* 1. What are you currently doing (in regard to your career) and how did you get there? */}
+      <span>Nyní již skoro 2 roky pracuji jako Data Analysta ve větší korporátní firmě. Předešlá práce je více related k programovacímu fieldu - byl jsem back-end C# programátor - ikdyž pouze na kratší dobu, naučilo mě to dost a otevřelo oči, jak věci fungují v reálném světě. Před touto zkušeností jsem znal programování pouze ze školy a konkrétně C# kompletně self-tought.</span>
+      {/* 2. In terms of the work you do, what aspects are you most passionate about and why? */}
+      <span>K programování se chci vrátit - konkrétně chci přesedlat na front-end, protože mám rád vizualizaci své práce. Jako hlavní library jsem si vybral React, který mi přišel ze všech možností nejsympatištější a práce s componentama a hookama mi příjde zajímavá a efficient.</span>
+      {/* 3. What do you consider some of your biggest professional and personal accomplishments? */}
+      {/* 4. What are you looking for right now? */}
+      <span>V dnešní době inflace a nejistoty hledám cokoliv v podobě brigády/part-time jako front-endista, než se v tomto fieldu pořádně rozkoukám a vzájemně zjistíme, zda nám vzájemná spolupráce sedí.</span>
     </>
   );
 }
